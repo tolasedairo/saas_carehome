e4ed8efbb2855a05e957552b5680d026a2fbb1db
tolase <tolasedairo@gmail.com>
2026-09-13 23:09:55 +0100

    feat: add care home tenancy model

--- src/tenancy/admin.py
+++ src/tenancy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CareHome
+
+
+@admin.register(CareHome)
+class CareHomeAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "is_active", "created_at")
+    list_filter = ("is_active",)
+    search_fields = ("name", "slug", "email")
